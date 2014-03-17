7fb3052e4a00819ef623f6709032e9f6bf9cee09
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-17 23:45:19 +0100

    Initial commit

---
+++ RxSQLNotify.Tests/RxSql_Specifications.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Reactive;
+using System.Threading;
+using Dapper;
+using FluentAssertions;
+using Machine.Specifications;
+
+namespace RxSQLNotify.Tests
+{
+	public class SqlConnectionSetup
+	{
+		const string ConnectionString = @"Data Source=(localdb)\Projects;Initial Catalog=TestNotify;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+		protected static SqlConnection Connection;
+		protected static IList<Unit> Calls;
+
+		Establish context = () =>
+		{
+			Connection = new SqlConnection(ConnectionString);
+			Calls = new List<Unit>();
+		};
+
+		Cleanup after = () => Connection.Execute("truncate table Test");
+	}
+
+	[Subject(typeof(SqlConnection))]
+	public class When_requesting_a_subscription_for_changes_on_a_table : SqlConnectionSetup
+	{
+		Because of = () =>
+		{
+			Connection.Observe("select Id, Name, BigText from dbo.Test").Subscribe(Calls.Add);
+			Connection.Execute("insert into Test (Name, BigText) values ('Value1', 'BigText1')");
+			Thread.Sleep(500);
+		};
+
+		It should_receive_a_notification_whenever_the_table_changes = () => Calls.Count.Should().Be(1);
+	}
+
+	[Subject(typeof(SqlConnection))]
+	public class When_requesting_a_subscription_for_changes_followed_by_subsequent_changes: SqlConnectionSetup
+	{
+		Because of = () =>
+		{
+			Connection.Observe("select Id, Name, BigText from dbo.Test").Subscribe(Calls.Add);
+			Connection.Execute("insert into Test (Name, BigText) values ('Value1', 'BigText1')");
+			Thread.Sleep(500);
+			Connection.Execute("insert into Test (Name, BigText) values ('Value2', 'BigText2')");
+			Thread.Sleep(500);
+			Connection.Execute("update Test set Name = 'NewValue' where Name = 'Value1'");
+			Thread.Sleep(500);
+		};
+
+		It should_receive_a_notification_for_every_change = () => Calls.Count.Should().Be(3);
+	}
+
+	[Subject(typeof(SqlConnection))]
+	public class When_requesting_a_subscription_for_changes_on_a_specific_column : SqlConnectionSetup
+	{
+		Because of = () =>
+		{
+			Connection.Observe("select BigText from dbo.Test").Subscribe(Calls.Add);
+			Connection.Execute("insert into Test (Name, BigText) values ('Value1', 'BigText1')");
+			Thread.Sleep(500);
+			Connection.Execute("update Test set Name = 'NewValue' where Name = 'Value1'");
+			Thread.Sleep(500);
+			Connection.Execute("update Test set BigText = 'NewValue' where Name = 'NewValue'");
+			Thread.Sleep(500);
+		};
+
+		It should_notify_for_the_requested_fields_only = () => Calls.Count.Should().Be(2);
+	}
+
+	[Subject(typeof(SqlConnection))]
+	public class When_requesting_a_subscription_for_a_table_containing_data : SqlConnectionSetup
+	{
+		Because of = () =>
+		{
+			Connection.Execute("insert into Test (Name, BigText) values ('Value1', 'BigText1')");
+			Connection.Execute("insert into Test (Name, BigText) values ('Value2', 'BigText2')");
+			Connection.Execute("insert into Test (Name, BigText) values ('Value3', 'BigText3')");
+			Connection.Execute("insert into Test (Name, BigText) values ('Value4', 'BigText4')");
+			Connection.Observe("select BigText from dbo.Test").Subscribe(Calls.Add);
+			Connection.Execute("delete from Test where Name = 'Value3'");
+			Thread.Sleep(500);
+		};
+
+		It should_notify_of_deleted_records_too = () => Calls.Count.Should().Be(1);
+	}
+}
---
+++ RxSQLNotify/RxSql.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace RxSQLNotify
+{
+	public static class RxSql
+	{
+		public static IObservable<Unit> Observe(this SqlConnection connection, string statement)
+		{
+			if((connection.State & ConnectionState.Open) == 0)
+				connection.Open();
+
+			return new RepeatableNotification(new SqlCommand(statement, connection))
+				.Do(sqlCommand => sqlCommand.ExecuteScalar())
+				.Select(_ => Unit.Default);
+		}
+
+		class RepeatableNotification : IObservable<SqlCommand>
+		{
+			readonly ISubject<SqlCommand> Notifications;
+			readonly string ConnectionString;
+			bool _dependencyStarted;
+
+			void StartSqlDependency()
+			{
+				if (_dependencyStarted)
+					return;
+
+				var principal = Thread.CurrentPrincipal;
+				Thread.CurrentPrincipal = null;
+
+				try
+				{
+					SqlDependency.Start(ConnectionString);
+					_dependencyStarted = true;
+				}
+				catch (Exception ex)
+				{
+					Trace.TraceError("Exception occurred while registering SQL Dependency: {0}", ex);
+					throw;
+				}
+				finally
+				{
+					Thread.CurrentPrincipal = principal;
+				}
+			}
+
+			public RepeatableNotification(SqlCommand template)
+			{
+				ConnectionString = template.Connection.ConnectionString;
+				StartSqlDependency();
+				Notifications = new Subject<SqlCommand>();
+				Notifications.Subscribe(SubscribeChange);
+				SubscribeChange(template);
+			}
+
+			void SubscribeChange(SqlCommand template)
+			{
+				var notification = new SqlDependency(template);
+				OnChangeEventHandler onChange = null;
+				onChange = (sender, args) =>
+				{
+					((SqlDependency)sender).OnChange -= onChange;
+					if (args.Type == SqlNotificationType.Subscribe)
+						Notifications.OnError(new ArgumentException("The command is not compliant with the notifications system or the database is not enabled for notifications."));
+					else
+						Notifications.OnNext(template.Clone());
+				};
+				notification.OnChange += onChange;
+				template.ExecuteScalar();
+			}
+
+			public IDisposable Subscribe(IObserver<SqlCommand> observer)
+			{
+				return new CompositeDisposable(Notifications.SubscribeSafe(observer), Disposable.Create(() => SqlDependency.Stop(ConnectionString)));
+			}
+		}
+	}
+}
